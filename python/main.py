2034230bcb1dbb2bd924467a7b52fffc0e33db5b
prat96 <pratheekb96@gmail.com>
2020-01-18 17:18:26 +0200

    Compute Circle enclosure co-ordinates

--- python/main.py
+++ python/main.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-def tests():
+
+def CircleDetection():
     img = cv2.imread('../oriental_picture.png', 0)
     img = cv2.medianBlur(img, 5)
     cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
@@ -15,13 +16,26 @@ def tests():
         cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
         # draw the center of the circle
         cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-        print(i[0], i[1], i[2])
 
+    points = [i[0], i[1], i[2]]
     cv2.imshow('detected circles', cimg)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    computeCircleEnclosure(points)
+
+
+def computeCircleEnclosure(circleData):
+    cx = circleData[0]
+    cy = circleData[1]
+    r = circleData[2]
+
+    x1 = ((cx - r), (cy - r))
+    x2 = ((cx + r), (cy + r))
+
+    print(x1, x2)
+
 
 if __name__ == '__main__':
     print("We go again!!")
-    tests()
+    CircleDetection()
